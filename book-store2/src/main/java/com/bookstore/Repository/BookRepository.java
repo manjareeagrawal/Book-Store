b55a0dd9dbe5d9aaf375c16d887e9aa47b29ee43
manjareeagrawal <121421458+manjareeagrawal@users.noreply.github.com>
2023-12-11 15:33:34 +0530

    Delete book-store2 directory

--- book-store2/src/main/java/com/bookstore/Repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.Repository;
-
-import com.bookstore.Entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book,Integer> {
-}
